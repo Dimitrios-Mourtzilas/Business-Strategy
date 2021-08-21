08089cfe5ed1e33e14df171abba91f7d8ef46454
Dimitrios-Mourtzilas <dimimour@students.kastoria.teiwm.gr>
2021-08-21 16:25:48 +0300

    changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import sys
 
 from PyQt5.QtCore import QThread
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from src.GUI.FIleDialog import FileDialog
 from src.GUI.LoginWindow import *
@@ -13,19 +13,14 @@ from src.Model.Database import Database
 from src.Model.User import *
 from src.Model.FinancialReport import *
 import pandas as pd
+
+
 def main():
-
-    mainFile = FileDialog()
-    mainFile.setDialog()
-
-    #excel_file = pd.read_excel("../company.xlsx")
-    #print(excel_file)
-    #financialReport = FinancialReport(excel_file)
-    #loginWindow = QMainWindow()
-    #loginUi = Ui_loginWindow()
-    #loginUi.setupUi(loginWindow)
-    #loginWindow.show()
-    #exit(app.exec())
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setupUi()
+    window.runUi()
+    exit(app.exec())
 
 
 if __name__ == "__main__":
