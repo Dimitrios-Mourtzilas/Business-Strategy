9d46d591692d95fa5ac225c04a6b5fa5c6a451b9
Dimitrios-Mourtzilas <dimimour@students.kastoria.teiwm.gr>
2021-11-30 10:29:51 +0200

    fixed algorithm plus modified xlsx file

--- src/Algorithm/DecisionTree.py
+++ src/Algorithm/DecisionTree.py
@@ -1,32 +1,34 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+import pandas
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import numpy
+from sklearn import tree
+from matplotlib import pyplot as plt
+class Tree:
 
-
-
-from src.Model.FinancialReport import *
-class DecisionTree:
-    _report = None
-    _x_array = []
-    _y_array = []
-    def __init__(self,**kwargs):
+    def __init__(self):
         pass
 
-    def setReport(self,report):
-        self.portable_formats = ['.xlsx','xltx']
-        self.file_extension = self.report.strip('.')
-        print(self.file_extension)
-        if not isinstance(report,FinancialReport):
-            return
-        self._report = report
+    def setprops(self):
+        self.file = "financial_folder/financial_statement_2019_2020.xlsx"
+        self.data = pandas.read_excel(self.file)
+        self.X = numpy.array(self.data.iloc[3:24,1]).reshape(-1,1)
+        self.y  = numpy.array(self.data.iloc[3:24,2]).reshape(-1,1)
+        print(self.X)
+        print("\n")
+        print(self.y)
 
-    def trainData(self):
-        self.data = pd.read_excel(self._report)
-        for i in range(6,16):
-            self._x_array.append(self.data.iloc[:27,i])
-            self.y_array.append(self.data.iloc[28,i])
-        self.x_train,self.x_test,self.y_train,self.y_test = train_test_split(self._x_array[0],self._y_array[0])
-        
+        self.tree = DecisionTreeRegressor(splitter="best")
+        self.x_train,self.x_test,self.y_train,self.y_test = train_test_split(self.X,self.y)
+        self.tree.fit(self.x_train,self.y_train)
+        self.y_pred = self.tree.predict(self.y_test)
+        print(self.y_pred)
+        print(self.tree.score(self.x_test,self.y_test))
+        self.dot_file = open("dot_tree.dot","w")
+        self.dot_file =tree.export_graphviz(self.tree,out_file=self.dot_file)
+             
+    def plotData(self):
+        plt.scatter(self.x_train,self.y_train,color="blue")
+        plt.scatter(self.x_test,self.y_test,color="red")
+        plt.show()
