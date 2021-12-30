4bd16001b90b4c52600c71f39dc4d72e8196a5ff
Dimitrios-Mourtzilas <dimimour@students.kastoria.teiwm.gr>
2021-12-30 17:53:04 +0200

    removed extra code

--- src/GUI/FIleProps.py
+++ src/GUI/FIleProps.py
@@ -84,7 +84,6 @@ class Ui_FileProps(QtWidgets.QWidget):
     def setFileProps(self,file):
         self.file = file
         self.file_prop_name_text.setText(os.path.basename(self.file.getFileName()))
-        self.file_prop_name_text.setText(self.file.getFileName())
         self.file_prop_size_text.setText(str(self.file.getFileSize()))
         self.file_prop_date_added_text.setText(self.file.getDateAdded())
 
